511975eeed9b64d45133a2c767b39a727749da4e
tomasvc <tomas.v0147@gmail.com>
2021-11-18 13:34:23 +0000

    Add favorites

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Grid, Typography, IconButton, Modal } from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPen, faTrash, faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
@@ -7,39 +7,15 @@ import TempContext from './TempContext'
 import Search from './Search'
 import axios from 'axios'
 
-export default function Location({ name, tempC, tempF, condition, isFavorite, onFavoriteClick, onDeleteClick }) {
+export default function Location({ name, tempC, tempF, condition, isFavorite, onFavoriteClick, onDeleteClick, editLocation }) {
 
     const {temp} = useContext(TempContext)
 
     const [open, setOpen] = useState(false)
-    const [data, setData] = useState({
-        name,
-        tempC,
-        tempF,
-        condition,
-        isFavorite
-    })
 
-    const onClick = () => {
-        onFavoriteClick()
-    }
-
-    const editLocation = (item) => {
-        axios(`https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=${item?.label}`)
-            .then(res => {
-                setData(prevState => ({ 
-                    ...prevState, 
-                    [name]: res.data.location.name,
-                    [tempC]: res.data.current.temp_c,
-                    [tempF]: res.data.current.temp_f,
-                    [condition]: res.data.current.condition.text
-                }))
-                // document.querySelector('.name').innerHTML = res.data.location.name
-                // document.querySelector('.temp').innerHTML = `${temp === 'C' ? res.data.current.temp_c : res.data.current.temp_f} &deg;${temp}`
-                // document.querySelector('.condition').innerHTML = res.data.current.condition.text
-            })
-        setOpen(false)
-    }
+    // const onClick = () => {
+    //     onFavoriteClick()
+    // }
 
     return (
         <>
@@ -56,7 +32,7 @@ export default function Location({ name, tempC, tempF, condition, isFavorite, on
                 <Typography className="temp" variant='h4' sx={{color: '#222'}}>{temp === 'C' ? tempC : tempF} &deg;{temp}</Typography>
                 <Typography className="condition" sx={{color: '#222', marginBottom: 1}}>{condition}</Typography> 
                 <Grid> 
-                    <IconButton size="small" onClick={() => onClick(name)}><FontAwesomeIcon icon={isFavorite ? solidHeart : regularHeart} style={{color: isFavorite && '#fc2d2d'}} /></IconButton>
+                    <IconButton size="small" onClick={() => onFavoriteClick()} ><FontAwesomeIcon icon={isFavorite ? solidHeart : regularHeart} style={{color: isFavorite && '#fc2d2d'}} /></IconButton>
                     <IconButton size="small" onClick={() => setOpen(true)}><FontAwesomeIcon sx={{marginRight: 2}} icon={faPen} style={{color: '#444'}} /></IconButton>
                     <IconButton size="small" onClick={() => onDeleteClick(name)}><FontAwesomeIcon icon={faTrash} style={{color: '#444'}} /></IconButton>
                 </Grid>
@@ -70,7 +46,7 @@ export default function Location({ name, tempC, tempF, condition, isFavorite, on
                     alignItems: 'center',
                     backgroundColor: 'background.paper',
                     borderRadius: 2,
-                    width: '30%',
+                    width: '50%',
                     height: '10%',
                     margin: 'auto',
                     padding: 5
@@ -79,7 +55,7 @@ export default function Location({ name, tempC, tempF, condition, isFavorite, on
             >
                 <div style={{zIndex: 10}}>
                     <Typography>Add new location name</Typography>
-                    <Search onAddLocation={editLocation} />
+                    <Search style={{width: '100%'}} onAddLocation={editLocation} />
                 </div>
             </Modal>
         </>
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -7,82 +7,74 @@ import Search from './Search'
 export default function Locations() {
 
     const [locations, setLocations] = useState(JSON.parse(localStorage.getItem('locations')))
-    const [favoriteLocations, setFavoriteLocations] = useState([])
+    const [favoriteLocations, setFavoriteLocations] = useState(JSON.parse(localStorage.getItem('favoriteLocations')))
 
     useEffect(() => {
         localStorage.setItem('locations', JSON.stringify(locations))
-    }, [locations])
+        localStorage.setItem('favoriteLocations', JSON.stringify(favoriteLocations))
+    }, [locations, favoriteLocations])
 
-    // const onFavoriteClick = (item) => {
-    //     let isItemInFavorites = favoriteLocations.some(el => el.location.location.name === item.location.location.name)
-    //     if (!isItemInFavorites) {
-    //         if (favoriteLocations.length === 0) {
-    //             item.isFavorite = true
-    //             setFavoriteLocations([item])
-    //             setLocations([item, ...locations.filter(el => el.location.location.name !== item.location.location.name)])
-    //         } else if (favoriteLocations.length > 0) {
-    //             item.isFavorite = true
-    //             setFavoriteLocations([item, ...favoriteLocations])
-    //             setLocations([...favoriteLocations, ...locations.filter(el => el.location.location.name !== item.location.location.name)])
-    //         }
-    //     } else {
-    //         item.isFavorite = false
-    //         setFavoriteLocations(favoriteLocations.filter(el => el.location.location.name !== item.location.location.name))
-    //         setLocations([...favoriteLocations, ...locations])
-    //     }
-    // }
-
-    const onEdit = (name) => {
-
+    const onFavoriteClick = (item) => {
+        let isItemInFavorites = favoriteLocations.some(el => el.location.location.name === item.location.location.name)
+        if (!isItemInFavorites) {
+            if (favoriteLocations.length === 0) {
+                item.isFavorite = true
+                setFavoriteLocations([item])
+                setLocations([...locations.filter(el => el.location.location.name !== item.location.location.name)])
+            } else if (favoriteLocations.length > 0) {
+                item.isFavorite = true
+                setFavoriteLocations([item, ...favoriteLocations])
+                setLocations([...locations.filter(el => el.location.location.name !== item.location.location.name)])
+            }
+        } else {
+            item.isFavorite = false
+            setFavoriteLocations(favoriteLocations.filter(el => el.location.location.name !== item.location.location.name))
+            setLocations([...locations])
+        }
     }
 
     const onDelete = (name) => {
-        setLocations(locations.filter(item => {
+        setLocations(locations?.filter(item => {
+            return item.location.location.name !== name
+        }))
+        setFavoriteLocations(favoriteLocations?.filter(item => {
             return item.location.location.name !== name
         }))
     }
 
-
-    // useEffect(() => {
-
-    //     axios.all([
-    //         axios('https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=London'),
-    //         axios('https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=Paris'),
-    //         axios('https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=New York'),
-    //         axios('https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=Tokyo')
-    //     ])
-    //     .then(response => {
-    //         let array = []
-    //         response.map(item => {
-    //             array = [...array, {location: item.data, isFavorite: false}]
-    //             return setLocations(array)
-    //         })
-    //     })
-
-    // }, [])
-
-    
     const addLocation = (item) => {
         axios(`https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=${item?.label}`)
             .then(res => {
-                if (locations?.length > 0) {
-                    if (locations?.some( el => el.location.location.name === res.data.location.name)) {
+                if (locations?.length > 0 || favoriteLocations?.length > 0) {
+                    if (locations?.some( el => el.location.location.name === res.data.location.name) ||
+                        favoriteLocations?.some( el => el.locations.locations.name === res.data.locations.name )) {
                         alert('This location already exists in your list')
                     } else {
                         setLocations([{location: res?.data, isFavorite: false}, ...locations]) 
                     }
                 } else {
-                    if (locations?.some( el => el.location.location.name === res.data.location.name)) {
+                    if (locations?.some( el => el.location.location.name === res.data.location.name ) ||
+                        favoriteLocations?.some( el => el.locations.locations.name === res.data.locations.name )) {
                         alert('This location already exists in your list')
                     } else {
                         setLocations([{location: res?.data, isFavorite: false}]) 
                     }
                 }
-                
-            })
-        
-            
+            })     
     }
+
+    // const editLocation = (item) => {
+    //     axios(`https://api.weatherapi.com/v1/current.json?key=b052758d6ca34a0ea45160442211011&q=${item?.label}`)
+    //         .then(res => {
+    //             let newList = locations?.filter(loc => loc.location.location.name !== res.data.location.name)
+    //             console.log(newList)
+    //             setLocations(() => ([ 
+    //                 ...newList, 
+    //                 { location: res?.data, isFavorite: false}
+    //             ]))
+    //             console.log(locations)
+    //         })
+    // }
 
 
     return (
@@ -95,6 +87,19 @@ export default function Locations() {
                     marginBottom: 5,
                     background: '#fafafa'
                 }}>
+                {favoriteLocations?.map((item) => {
+                    return <Location 
+                    key={item.location.location.name} 
+                    name={item.location.location.name} 
+                    tempC={item.location.current.temp_c} 
+                    tempF={item.location.current.temp_f} 
+                    condition={item.location.current.condition.text}
+                    isFavorite={item.isFavorite}
+                    // editLocation={editLocation}
+                    // onFavoriteClick={() => onFavoriteClick(item)} 
+                    onDeleteClick={onDelete} 
+                />
+                })}
                 {locations?.map((item) => {
                     return <Location 
                                 key={item.location.location.name} 
@@ -103,7 +108,8 @@ export default function Locations() {
                                 tempF={item.location.current.temp_f} 
                                 condition={item.location.current.condition.text}
                                 isFavorite={item.isFavorite}
-                                // onFavoriteClick={() => onFavoriteClick(item)} 
+                                // editLocation={editLocation}
+                                onFavoriteClick={() => onFavoriteClick(item)} 
                                 onDeleteClick={onDelete} 
                             />
                 })}
